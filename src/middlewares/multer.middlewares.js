5fe0f38ec6d796d08f0dd7bd73f5705ac8bf94a4
Ajmeri Gudiya <149288719+Ajmeri799@users.noreply.github.com>
2024-12-06 23:56:33 +0530

    multer.middlewares.js

--- src/middlewares/multer.middlewares.js
+++ src/middlewares/multer.middlewares.js
@@ -3,6 +3,8 @@ const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
   },
+
+  
   filename: function (req, file, cb) {
     // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, Date.now() + "-" + file.originalname);
