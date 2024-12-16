a641fc998406f6e9fe896320af9e36385d26f008
Ajmeri Gudiya <149288719+Ajmeri799@users.noreply.github.com>
2024-12-16 23:48:39 +0530

    Update healthcheck.controllers.js

--- src/controllers/healthcheck.controllers.js
+++ src/controllers/healthcheck.controllers.js
@@ -1,7 +1,6 @@
 import { asyncHandler } from "../utils/acyncHandler.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-const healthcheckRouter = asyncHandler(async (req, res) => {
-  
+const healthcheckRouter = asyncHandler(async (req, res) => { 
   return res
     .status(200)
     .json(new ApiResponse(200, "OK", "Health check passed"));
